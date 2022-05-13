cb059bd1218d85eb09d475e606ba56268d56b102
wazxn <zvp.ukr01@gmail.com>
2022-05-13 15:03:35 +0300

    2.1

--- mhddos.bat
+++ mhddos.bat
@@ -1,2 +1,19 @@
-start "mhddos" powershell -noexit -executionpolicy bypass -command "& '%USERPROFILE%\Desktop\mhddos.ps1'"
+@echo off
+setlocal
+for /f "tokens=2 delims=[]" %%i in ('ver') do set VERSION=%%i
+for /f "tokens=2-3 delims=. " %%i in ("%VERSION%") do set VERSION=%%i.%%j
+if "%VERSION%" GEQ "6.1" goto w7
+endlocal
+if "%VERSION%" == "5.00" set "w=Windows 2000"
+if "%VERSION%" == "5.0" set "w=Windows 2000"
+if "%VERSION%" == "5.1" set "w=Windows XP"
+if "%VERSION%" == "5.2" set "w=Windows Server 2003"
+if "%VERSION%" == "6.0" set "w=Windows Vista"
+echo Your %w% is unsupported
+pause
+goto end
+
+:w7
+start "mhddos" powershell -noexit -executionpolicy bypass -command "if ($PSVersionTable.PSVersion.Major -ge 3) {[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; cls; (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/wvzxn/mhddos-proxy-py/main/mhddos.ps1') | iex; exit} else {Write-Host "Your Windows is obsolete, please read this"; Start-Sleep -s 5; cls; Start-Process "https://github.com/wvzxn/mhddos-proxy-py" ; exit}"
+:end
 exit
--- mhddos.ps1
+++ mhddos.ps1
@@ -1,15 +1,42 @@
 # ------------------------- Settings ------------------------
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
 [Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("cp866")
 [double]$OS = ([string][System.Environment]::OSVersion.Version.Major) + "." + ([string][System.Environment]::OSVersion.Version.Minor)
-if ($OS -lt 6.1) {$oldOS = (Get-WmiObject -class Win32_OperatingSystem).Caption; Write-Output "Your Windows version is obsolete ($oldOS)"; Start-Sleep -s 5; Exit}
-if ($PSVersionTable.PSVersion.Major -lt 5) {$PSold = $true} else {$PSold = $false}
 if ((Get-WmiObject win32_operatingsystem).osarchitecture -like "*64*") {$OS64bit = $true} else {$OS64bit = $false}
 Write-Output "OS = $OS | PSold = $PSold | OS64bit = $OS64bit"
 Get-ExecutionPolicy -List
 
-echo "press any key..." ; [void][Console]::ReadKey($true).Key
+if ($PSVersionTable.PSVersion.Major -lt 5) {Update-Help -force -erroraction silentlycontinue}
+echo "натисніть ...." ; [void][Console]::ReadKey($true).Key
 
-Update-Help -force -erroraction silentlycontinue
+# global variables
+$global:lastpercentage = -1
+$global:are = New-Object System.Threading.AutoResetEvent $false
+
+# web client
+# (!) output is buffered to disk -> great speed
+$wc = New-Object System.Net.WebClient
+
+Register-ObjectEvent -InputObject $wc -EventName DownloadProgressChanged -Action {
+    # (!) getting event args
+    $percentage = $event.sourceEventArgs.ProgressPercentage
+    if($global:lastpercentage -lt $percentage)
+    {
+        $global:lastpercentage = $percentage
+        # stackoverflow.com/questions/3896258
+        Write-Host -NoNewline "`r$percentage%"
+    }
+} > $null
+
+Register-ObjectEvent -InputObject $wc -EventName DownloadFileCompleted -Action {
+    $global:are.Set()
+    Write-Host
+} > $null
+
+$wc.DownloadFileAsync("https://raw.githubusercontent.com/wvzxn/mhddos-proxy-py/main/vcr.zip","$env:userprofile\Desktop\vcr.zip");
+# ps script runs probably in one thread only (event is reised in same thread - blocking problems)
+# $global:are.WaitOne() not work
+while(!$global:are.WaitOne(500)) {}
 
 # (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/wvzxn/mhddos-proxy-py/main/vcr.zip","$env:userprofile\Desktop\vcr.zip")
 # & "$env:userprofile\Desktop\7za.exe" e -y "-pwz" "vcr.zip"
@@ -26,4 +53,8 @@ $now = (get-date) ; $future = (get-date).AddSeconds($time)
 Write-Output "Job start: $now" ` "Job end: $future" ; Start-Sleep -s $time
 if ($Shutdown -eq $true) {Stop-Computer -ComputerName localhost}
 }
+
+
+
+
 #>
