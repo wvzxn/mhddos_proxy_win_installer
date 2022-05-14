42ef0933d5d92fabbdb0a3925d62f6b1b9ff1cd6
wazxn <zvp.ukr01@gmail.com>
2022-05-14 22:39:50 +0300

    v0.22 - робота з аддонами продовжується

--- mhddos.bat
+++ mhddos.bat
@@ -1,19 +1,52 @@
 @echo off
-setlocal
-for /f "tokens=2 delims=[]" %%i in ('ver') do set VERSION=%%i
-for /f "tokens=2-3 delims=. " %%i in ("%VERSION%") do set VERSION=%%i.%%j
-if "%VERSION%" GEQ "6.1" goto w7
-endlocal
-if "%VERSION%" == "5.00" set "w=Windows 2000"
-if "%VERSION%" == "5.0" set "w=Windows 2000"
-if "%VERSION%" == "5.1" set "w=Windows XP"
-if "%VERSION%" == "5.2" set "w=Windows Server 2003"
-if "%VERSION%" == "6.0" set "w=Windows Vista"
-echo Your %w% is not supported
-pause
-goto end
 
-:w7
-start "mhddos" powershell -noexit -executionpolicy bypass -command "if ($PSVersionTable.PSVersion.Major -ge 3) {[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; cls; (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/wvzxn/mhddos-proxy-py/main/mhddos.ps1') | iex; exit} else {Write-Host "Your Windows is obsolete, please read this"; Start-Sleep -s 5; cls; Start-Process "https://github.com/wvzxn/mhddos-proxy-py" ; exit}"
+:: Old Windows check
+for /f "tokens=4-5 delims=. " %%i in ('ver') do set VERSION=%%i%%j
+if %VERSION% GTR 60 goto:dotnet
+echo Your Windows is not supported
+pause
+goto:end
+
+:: .NET Framework 4.5+ check
+:dotnet
+setlocal
+@reg query "HKLM\Software\Microsoft\NET Framework Setup\NDP\v4\Full" >nul
+if %errorlevel% EQU 0 endlocal & goto:pwsh
+echo "Missing .NET Framework 4.5+"
+pause
+
+:: .NET Framework 4.5.2 Installer
+echo "// Downloading . . ."
+call:mkdir-temp
+powershell -executionpolicy bypass -command "[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/wvzxn/mhddos-proxy-py/raw/main/addons/7za.exe','%folder%\7za.exe'); (New-Object System.Net.WebClient).DownloadFile('https://download.microsoft.com/download/E/2/1/E21644B5-2DF2-47C2-91BD-63C560427900/NDP452-KB2901907-x86-x64-AllOS-ENU.exe','%folder%\NDP452-KB2901907-x86-x64-AllOS-ENU.exe'); & '%folder%\7za.exe' e -y "-pwz" "vcr.zip""
+
+:: Powershell version check
+:pwsh
+setlocal
+powershell -executionpolicy bypass -command "if ($PSVersionTable.PSVersion.Major -ge 3) {exit 1} else {exit 2}"
+if %errorlevel% EQU 1 endlocal & goto:ps1
+echo "errorlevel = %errorlevel%"
+pause
+
+:: powershell -executionpolicy bypass -command "[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; cls; (New-Object System.Net.WebClient).DownloadFile('https://github.com/wvzxn/mhddos-proxy-py/raw/main/vcr/vcr.zip', '%USERPROFILE%\Desktop\vcr.zip')"
+pause
+goto:end
+
+:ps1
+echo ps1
+pause
+:: start "mhddos" powershell -executionpolicy bypass -command "[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; cls; (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/wvzxn/mhddos-proxy-py/main/mhddos.ps1') | iex"
+
 :end
 exit
+
+:: Temp folder
+
+:mkdir-temp
+set "folder=%TMP%\mhddos-temp\"
+if not exist "%folder%" mkdir "%folder%"
+exit /b
+
+:del-temp
+if exist "%folder%" rd /s /q "%folder%"
+exit /b
--- mhddos.ps1
+++ mhddos.ps1
@@ -4,39 +4,10 @@
 [double]$OS = ([string][System.Environment]::OSVersion.Version.Major) + "." + ([string][System.Environment]::OSVersion.Version.Minor)
 if ((Get-WmiObject win32_operatingsystem).osarchitecture -like "*64*") {$OS64bit = $true} else {$OS64bit = $false}
 Write-Output "OS = $OS | PSold = $PSold | OS64bit = $OS64bit"
-Get-ExecutionPolicy -List
+# Get-ExecutionPolicy -List
 
-if ($PSVersionTable.PSVersion.Major -lt 5) {Update-Help -force -erroraction silentlycontinue}
+# if ($PSVersionTable.PSVersion.Major -lt 5) {Update-Help -force -erroraction silentlycontinue}
 echo "натисніть ...." ; [void][Console]::ReadKey($true).Key
-
-# global variables
-$global:lastpercentage = -1
-$global:are = New-Object System.Threading.AutoResetEvent $false
-
-# web client
-# (!) output is buffered to disk -> great speed
-$wc = New-Object System.Net.WebClient
-
-Register-ObjectEvent -InputObject $wc -EventName DownloadProgressChanged -Action {
-    # (!) getting event args
-    $percentage = $event.sourceEventArgs.ProgressPercentage
-    if($global:lastpercentage -lt $percentage)
-    {
-        $global:lastpercentage = $percentage
-        # stackoverflow.com/questions/3896258
-        Write-Host -NoNewline "`r$percentage%"
-    }
-} > $null
-
-Register-ObjectEvent -InputObject $wc -EventName DownloadFileCompleted -Action {
-    $global:are.Set()
-    Write-Host
-} > $null
-
-$wc.DownloadFileAsync("https://github.com/wvzxn/mhddos-proxy-py/raw/main/vcr/vcr.zip","$env:userprofile\Desktop\vcr.zip");
-# ps script runs probably in one thread only (event is reised in same thread - blocking problems)
-# $global:are.WaitOne() not work
-while(!$global:are.WaitOne(500)) {}
 
 # (New-Object System.Net.WebClient).DownloadFile("https://github.com/wvzxn/mhddos-proxy-py/raw/main/vcr/vcr.zip","$env:userprofile\Desktop\vcr.zip")
 # & "$env:userprofile\Desktop\7za.exe" e -y "-pwz" "vcr.zip"
