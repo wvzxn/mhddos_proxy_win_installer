544df50b180a90dcb891f9d29632b03b5807094f
wazxn <zvp.ukr01@gmail.com>
2022-05-14 23:49:43 +0300

    v0.23

--- mhddos.bat
+++ mhddos.bat
@@ -1,24 +1,23 @@
 @echo off
 
-:: Old Windows check
+:: Old [Windows] check
 for /f "tokens=4-5 delims=. " %%i in ('ver') do set VERSION=%%i%%j
 if %VERSION% GTR 60 goto:dotnet
-echo Your Windows is not supported
+echo "Your [Windows] is not supported"
 pause
 goto:end
 
-:: .NET Framework 4.5+ check
+:: [.NET Framework 4.5] check
 :dotnet
 setlocal
 @reg query "HKLM\Software\Microsoft\NET Framework Setup\NDP\v4\Full" >nul
 if %errorlevel% EQU 0 endlocal & goto:pwsh
-echo "Missing .NET Framework 4.5+"
+echo "Missing [.NET Framework 4.5+]"
 pause
-
-:: .NET Framework 4.5.2 Installer
 echo "// Downloading . . ."
 call:mkdir-temp
-powershell -executionpolicy bypass -command "[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/wvzxn/mhddos-proxy-py/raw/main/addons/7za.exe','%folder%\7za.exe'); (New-Object System.Net.WebClient).DownloadFile('https://download.microsoft.com/download/E/2/1/E21644B5-2DF2-47C2-91BD-63C560427900/NDP452-KB2901907-x86-x64-AllOS-ENU.exe','%folder%\NDP452-KB2901907-x86-x64-AllOS-ENU.exe'); & '%folder%\7za.exe' e -y "-pwz" "vcr.zip""
+if not exist "%folder%\NDP452-KB2901907-x86-x64-AllOS-ENU" mkdir "%folder%\NDP452-KB2901907-x86-x64-AllOS-ENU"
+powershell -executionpolicy bypass -command "[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://github.com/wvzxn/mhddos-proxy-py/raw/main/addons/7z.exe','%folder%\7z.exe'); (New-Object System.Net.WebClient).DownloadFile('https://github.com/wvzxn/mhddos-proxy-py/raw/main/addons/7z.dll','%folder%\7z.dll'); (New-Object System.Net.WebClient).DownloadFile('https://download.microsoft.com/download/E/2/1/E21644B5-2DF2-47C2-91BD-63C560427900/NDP452-KB2901907-x86-x64-AllOS-ENU.exe','%folder%\ndp452.exe'); & '%folder%\7z.exe' e -y -o'%folder%\NDP452-KB2901907-x86-x64-AllOS-ENU' '%folder%\ndp452.exe'> $null"
 
 :: Powershell version check
 :pwsh
