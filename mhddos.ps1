90476d40febfc5f2559d94b7b1f9d6e45dcb16eb
wazxn <zvp.ukr01@gmail.com>
2022-05-19 15:00:46 +0300

    v0.24 - update

--- mhddos.ps1
+++ mhddos.ps1
@@ -1,22 +1,36 @@
 # ------------------------- Settings ------------------------
-[Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("cp866")
-[double]$_OS = ([string][System.Environment]::OSVersion.Version.Major) + "." + ([string][System.Environment]::OSVersion.Version.Minor)
-if ((Get-WmiObject win32_operatingsystem).osarchitecture -like "*64*") {$_OS64bit = $true} else {$_OS64bit = $false}
+# [Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("cp866")
+# [double]$_OS = ([string][System.Environment]::OSVersion.Version.Major) + "." + ([string][System.Environment]::OSVersion.Version.Minor)
+# if ((Get-WmiObject win32_operatingsystem).osarchitecture -like "*64*") {$_OS64bit = $true} else {$_OS64bit = $false}
 # Write-Output "OS = $OS | PSold = $PSold | OS64bit = $OS64bit"
-echo "натисніть ...." ; [void][Console]::ReadKey($true).Key
+# echo "натисніть ...." ; [void][Console]::ReadKey($true).Key
 # -----------------------------------------------------------
 
 $WC = New-Object System.Net.WebClient
 
 $folder = "$env:tmp\mhddos-temp"
 
+function Add-AddonsFolder {
+    param (
+        [string]$fldr
+    )
+    New-Item -Path "$fldr\addons" -Type Folder
+    $WC.DownloadFile("https://github.com/wvzxn/mhddos-proxy-py/raw/main/addons/7z.dll","$fldr\addons\7z.dll")
+    $WC.DownloadFile("https://github.com/wvzxn/mhddos-proxy-py/raw/main/addons/7z.exe","$fldr\addons\7z.exe")
+    $WC.DownloadFile("https://github.com/wvzxn/mhddos-proxy-py/raw/main/addons/vcr.zip","$fldr\addons\vcr.zip")
+}
+function Set-VCR {
+    param (
+        [string]$fldr
+    )
+    if (!(Test-Path -Path "$fldr\addons")) {
+        if (!(Test-Path -Path "$fldr")) {New-Item -Path "$fldr" -Type Folder}
+        Add-AddonsFolder "$fldr"
+    }
+    Start-Process -FilePath "$fldr\addons\7z.exe" -ArgumentList "e -y "-pwz" "$fldr\addons\vcr.zip""
+}
 # -----------------------------------------------------------
-
-$WC.DownloadFile("https://github.com/wvzxn/mhddos-proxy-py/raw/main/addons/7z.dll","$folder\7z.dll")
-$WC.DownloadFile("https://github.com/wvzxn/mhddos-proxy-py/raw/main/addons/7z.exe","$folder\7z.exe")
-$WC.DownloadFile("https://github.com/wvzxn/mhddos-proxy-py/raw/main/addons/vcr.zip","$folder\vcr.zip")
-& "$folder\7z.exe" e -y "-pwz" "vcr.zip"
-
+Set-VCR "$folder"
 echo "натисніть ...." ; [void][Console]::ReadKey($true).Key
 exit
 
